ed24fff6ac12a62c0be9fbf64daf2da580b125b7
Mashaal Musleh <meshalawy@gmail.com>
2016-04-18 14:54:20 +0300

    trajectories using path finding.js

---
+++ js/parse.js
@@ -0,0 +1,4 @@
+$.getScript("http://www.parsecdn.com/js/parse-1.6.14.min.js").done(function (){
+	debugger
+	Parse.initialize("pey8pjhhotyDIusYHgSEF6SUI84ugMPVe6rqF2Ah", "I8CbFbjMrKNKKauFicygVWXbe0V1V7tixOqzQpcX");	
+})
